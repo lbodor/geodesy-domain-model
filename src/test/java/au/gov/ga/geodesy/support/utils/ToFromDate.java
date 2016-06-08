6b13da3d1a7b348ccda0337e303752c5a0473300
Brooke Smith <brooke.smith@ga.gov.au>
2016-06-08 16:48:22 +1000

    Implement RadioInterference domain model mapper
    * Changed tests to use Hamcrest (and am not using the static
    imports but the class since it makes it much easier to resolve
    methods, otherwise you need to go copy and paste the static imports.

---
+++ src/test/java/au/gov/ga/geodesy/support/utils/ToFromDate.java
@@ -0,0 +1,10 @@
+package au.gov.ga.geodesy.support.utils;
+
+/**
+ * Represents the direction of mapping, for example XML types to JPA entities
+ * or vice versa.
+ */
+public enum ToFromDate {
+    TO_DATE,
+    FROM_DATE
+}
