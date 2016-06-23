1ff1e12a9b48805e667b3e6f771b6a47cc2ddf5e
Brooke Smith <brooke.smith@ga.gov.au>
2016-06-24 09:29:27 +1000

    RadioInterferenceMapper fixed bug
    * After recent refactoring change the ChangeTrackingDecoratorTest
    was failing

--- src/main/java/au/gov/ga/geodesy/support/mapper/decorator/GeodesyMLDecorators.java
+++ src/main/java/au/gov/ga/geodesy/support/mapper/decorator/GeodesyMLDecorators.java
@@ -135,7 +135,7 @@ public class GeodesyMLDecorators {
         private static <C> TimePositionType createTimePositionType(C childElement) throws ParseException {
             TimePositionType tpt = new TimePositionType();
             Method getter = null;
-            Instant useThisDate = Instant.now();
+            Instant useThisDate = null;
             try {
                 getter = childElement.getClass().getMethod("getDateInstalled");
                 useThisDate = buildInstant(childElement, getter);
@@ -146,7 +146,7 @@ public class GeodesyMLDecorators {
                 logger.error("Trying to get method getDateInstalled() on element: "
                         + childElement.getClass().getSimpleName() + "; just use now() instead", e);
             }
-
+            useThisDate = useThisDate != null ? useThisDate :Instant.now();
             tpt.setValue(Stream.of(GEODESYML_DATE_FORMAT_TIME_SEC.format(useThisDate))
                     .collect(Collectors.toList()));
             return tpt;
